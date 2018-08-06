4af9f18c21091a47386df19d07e1187b6a5239bb
kunal-mohta <kunalmohta1818@gmail.com>
2018-08-06 23:11:46 +0530

    ACS-1: Fix for error at the end of contract tests when API keys absent

--- share/translation/tests/translation-contract-tests.js
+++ share/translation/tests/translation-contract-tests.js
@@ -1,9 +1,12 @@
 "use strict";
 
-var fluid = require("infusion"),
-    kettle = require("kettle");
+require("../../utils");
 
-kettle.loadTestingSupport();
+var fluid = require("infusion"),
+    kettle = require("kettle"),
+    ACS = fluid.registerNamespace("ACS");
+
+require("dotenv").config();
 
 var testIncludes = [
     "./contractTests/google/detectAndTranslate.js",
@@ -14,6 +17,37 @@ var testIncludes = [
     "./contractTests/yandex/textTranslation.js"
 ];
 
-fluid.each(testIncludes, function (path) {
-    require(path);
-});
+/* The following terminates tests if API keys are not present
+ * for any of the translation services
+ * to avoid unnecessary nodejs error when this file is run
+ */
+
+// Check for keys
+var translationContractTestKeys = [
+    "YANDEX_API_KEY",
+    "GOOGLE_API_KEY"
+];
+
+var hasAnyValidKeys = function (expectedKeys) {
+    var anyValidKeys = false;
+
+    fluid.each(expectedKeys, function (key) {
+        var resolvedKey = kettle.resolvers.env(key);
+        if (resolvedKey) {
+            anyValidKeys = true;
+        }
+    });
+
+    return anyValidKeys;
+};
+
+if (hasAnyValidKeys(translationContractTestKeys)) {
+    kettle.loadTestingSupport();
+
+    fluid.each(testIncludes, function (path) {
+        require(path);
+    });
+}
+else {
+    ACS.log("No API keys were found for any translation service, cannot run contract tests");
+};
